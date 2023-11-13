1248c80feadd257adb3457c7097e7c40168d6457
Eliza <petryckieliza921@gmail.com>
2023-11-13 09:04:52 +0100

    Dodaj pliki projektów.

---
+++ kuestion/My Project/AssemblyInfo.vb
@@ -0,0 +1,35 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' Ogólne informacje o zestawie są kontrolowane poprzez następujący 
+' zestaw atrybutów. Zmień wartości tych atrybutów, aby zmodyfikować informacje
+' powiązane z zestawem.
+
+' Sprawdź wartości atrybutów zestawu
+
+<Assembly: AssemblyTitle("kuestion")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("kuestion")>
+<Assembly: AssemblyCopyright("Copyright ©  2023")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'Następujący identyfikator GUID jest identyfikatorem elementu typelib w przypadku udostępnienia tego projektu w modelu COM
+<Assembly: Guid("2f6b5a5f-e2e2-480b-99ef-1fb633797b09")>
+
+' Informacje o wersji zestawu zawierają następujące cztery wartości:
+'
+'      Wersja główna
+'      Wersja pomocnicza
+'      Numer kompilacji
+'      Poprawka
+'
+' Możesz określić wszystkie wartości lub użyć domyślnych numerów kompilacji i poprawki
+' przy użyciu symbolu „*”, tak jak pokazano poniżej:
+' <Assembly: AssemblyVersion("1.0.*")>
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
